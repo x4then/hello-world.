fa6507e5d73554880d71f0d5b49f6e06511598ff
x4then <phachjamp@gmail.com>
2025-04-11 20:15:28 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,25 @@
-public class zZzWtfClass {
+public class Main {
 
-public static void main(String[] aRgS){
-int[] lolz = {5,3,8,1,2};
-sorterBRO(lolz);
-for(int j=0;j<lolz.length;j++)
-System.out.print(lolz[j]+" ");
+    public static void main(String[] args) {
+        int[] numbers = {5, 3, 8, 1, 2};
+        sortAscending(numbers);
+        
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    // Sorts the array in ascending order using bubble sort
+    public static void sortAscending(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Swap elements
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
-
-// this function sorts stuff idk
-public static void sorterBRO(int[] daNumz){
-for(int i=0;i<daNumz.length-1;i++){
-    for(int j=0;j<daNumz.length;j++){ // BUG: should be j < daNumz.length - i - 1
- if(daNumz[j]<daNumz[j+1]){ // BUG: this is descending, not ascending
-int x=daNumz[j];
-daNumz[j]=daNumz[j+1];
-daNumz[j+1]=x;}
-}}}
